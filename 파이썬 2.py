da68f14ba9d30e6b61a92466891e86f603603f8b
김범서 <k22899142@gmail.com>
2026-07-02 16:55:30 +0900

    Add files via upload

---
+++ 파이썬 2.py
@@ -0,0 +1,152 @@
+##print("48 > %d %o" % (48,48),end="")
+##print("48 > %d %o" % (48,48))
+##print("%c %c" % (48,48))
+##print("%c %c" % (66,0x42))
+##print("%f %e" % (0.132, 0.132))
+##
+##print("이샘%s학원" % ("코딩"))
+##print("이샘", "학원",sep="코딩")
+##
+
+
+##num1=17
+##print(num1)
+##print("10진수 17 = %d"%(num1))
+##print("10진수 17의 8진수 = %o"%(num1))
+##print("10진수 17의 16진수 = %x"%(num1))
+##
+##num2 = 97
+##print("%d %c"%(num2,num2))
+
+##float1=1234.567
+##print(float1)
+##print("기본 출력 %f" %(float1))
+##print("지수표기법 %e" % (float1))
+##print("소수 둘째자리까지 %.2f" %(float1))
+
+##txt1="Hello"
+##txt2="World!"
+##
+##print("기본 출력\n%s %s" % (txt1, txt2))
+##print("7칸 출력\n%7s %7s" % (txt1, txt2))
+
+##txt1="Hello"
+##txt2="World!"
+##
+##print("기본 출력\n%s %s" % (txt1, txt2))
+##print("150칸 출력 \n%150s \n%150s" % (txt1, txt2))
+##
+
+##print("48 > {0} {1:o}".format(48,48))
+##print("48 > {0:x} {1:c}".format(48,48))
+##print("{0:c} {1:c}".format(65,0x41))
+##print("{0:f} {1:e}".format(0.132,0.132))
+##print("이샘{4}학원".format("고딩", "꼬딩", "구딩", "코팅", "코딩", "고팅"))
+
+##number = 48
+##hexA = 0x41
+##coding="코딩"
+##
+##print(f"48 > {number} {number:o}")
+##print(f"48 > {number:x} {number:c}")
+##print(f"{hexA} {hexA:c}")
+##print(f"{0.132}     {0.321:e}")
+##print(f"{"김범서는"} {"스무살"}")
+##print(f"이샘 {coding} 학원")
+##
+##print(f"김범서는 스무살")
+
+
+##num1=17
+##num2=98
+
+##print(num1)
+##print("10진수 17 = {0}".format(num1))
+##print("10진수 17 = {1}".format(num1, num2))
+##print("10진수 17의 16진수 = {0:x}".format(num1, num2))
+
+##num2 = 98
+##print("{0} {1:c}".format(num2, num2))
+##
+##float1=1234.567
+##print(float1)
+##print("기본 출력 {0:f}".format(float1))
+##print("지수표기법 {0:e}".format(float1))
+##print("소수 둘째자리 까지 {0:.2f}".format(float1))
+
+##txt1 = "Hello"
+##txt2 = "World!"
+##
+##print("기본 출력\n{0:s} {1:s}".format(txt1, txt2))
+##print("7칸 출력\n{0:7s} {1:7s}?".format(txt1, txt2))
+
+##num1 = 17
+##print(num1)
+##print(f'10진수 17 = {num1}')
+##print(f'10진수 17의 8진수 = {num1:o}')
+##print(f'10진수 17의 16진수 = {num1:x}')
+
+##num2 = 100
+##print(f"{num2} {num2:c}")
+##
+##float1=1234.567
+##print(float1)
+##print(f"기본 출력 {float1:f}")
+##print(f"지수 표기법 {float1:e}")
+##print(f"소수 둘째 자리까지 {float1:.2f}")
+
+##txt1="Hello"
+##txt2="World!"
+##
+##print(f'기본 출력\n{txt1:s} {txt2:s}')
+##print(f'7칸 출력\n{txt1:7s} {txt2:s}?')
+##print(f'15칸 출력\n{txt1:15s} {txt2:15s}?')
+
+
+###문제1)
+##r = 5
+##pi = 3.14
+##print('넓이는 %d * %d * %.2f = %.1f' % (r,r,pi,r*r*pi))
+##
+##
+##
+###문제2)
+##r = 5
+##pi = 3.14
+##print('넓이는 {0} * {0} * {1} = {2}'.format(r,pi,r*r*pi))
+##
+##
+###문제3)
+##r = 5
+##pi = 3.14
+##print(f'넓이는 {r} * {r} * {pi} = {r*r*pi}')
+
+
+###문제4)
+##golden_ratio = 1.61804
+##print(f'소수 첫째 자리까지 출력 : {golden_ratio:.1f}')
+##print(f'소수 둘째 자리까지 출력 : {golden_ratio:.2f}')
+##print(f'소수 셋째 자리까지 출력 : {golden_ratio:.3f}')
+##print(f'소수 넷째 자리까지 출력 : {golden_ratio:.4f}')
+
+
+passwd = input("비밀번호 : ")
+print(input("id : "),"입력")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
