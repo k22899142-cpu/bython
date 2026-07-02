2cfd9883fdf3561557a9a71afb97805376c3cf22
김범서 <k22899142@gmail.com>
2026-07-02 15:03:29 +0900

    Add files via upload

---
+++ 파이썬1.py
@@ -0,0 +1,86 @@
+##print('따옴표는 "역할'이 같아요")
+##print("따옴표는 "역할'이 같아요")
+##      
+##
+##apple = "사과"
+##print(apple, 1, "상자")
+
+##apple = "사과"
+##print(apple,1,"상자")
+
+
+
+##apple = "사과"
+##print(apple, 1, "상자", sep="++")
+##
+##apple = "사과"
+##print(apple,1,"상자",sep="++")
+##
+##apple = "사과"
+##print(apple, 1, "상자", sep="++")
+
+##print("이샘")
+##print("코딩")
+##print("학원")
+
+##print("이샘",end="")
+##print("코딩",end="")
+##print("학원",end="")
+
+##number = 10
+##pi = 3.14
+##name = '이샘'
+##check = True
+##
+##month = 10
+##my_score = 'A'
+##MySCORE = 'B'
+####1st = "이샘"
+##
+##print(type(month))
+
+##num1 = 10
+##print(num1, type(num1))
+
+##num2 = 3.14
+##print(num2, type(num2))
+##
+##m_str = "안녕하세요"
+##print(m_str, type(m_str))
+
+##print(1+1,type(1+1))
+##
+##print(0.3+2.04, type(0.3+2.04))
+
+##print(1+2.3, type(1+2.3))
+##
+##print("\"이샘코딩학원\"")
+##
+##print(1,2,3,sep="+",end="=6")
+##
+##print(f">{'abc':5s}<")
+##print(f">{3.141592:6.2f}<")
+
+
+##print("48 > %d %o" % (48,48),end="")
+##print("48 > %x %c" % (48, 48))
+##print("%c %c" % (65,0x41))
+##print("%c %c" % (66, 0x42))
+##print("%f %e" % (0.132, 0.132))
+##print("이샘%s학원"%("코딩"))
+
+  
+##num1 = 17
+##print(num1)
+##print("10진수 17 = %d"%(num1))
+##print("10진수 17의 8진수 = %o"%(num1))
+##print("10진수 17의 16진수 = %x"%(num1))
+
+num2 = 97
+print("%d %c" % (num2,num2))
+
+float=1234.567
+print("기본 출력 %f" % (float1))
+print("지수표기법" %e" % (float))
+print("소수 둘째자리까지 %.2f % (float))
+      
