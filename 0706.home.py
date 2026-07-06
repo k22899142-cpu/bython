fb56be05cf0910fe90d0cae4a63eb9af8c82ee6c
김범서 <k22899142@gmail.com>
2026-07-07 00:18:27 +0900

    Add files via upload

---
+++ 0706.home.py
@@ -0,0 +1,27 @@
+##name = str(input("이름을 입력해주세요 : "))
+##print("안녕하세요", name + "님!")
+
+
+##egg = float(input("계란은 몇 개?"))
+##print(egg, type(egg))
+
+##attack = int(input("공격력은? "))
+##sheld = int(input("방어력은? "))
+##
+##print("공격력과 방어력을 곱한 값은", attack * sheld, "입니다!")
+
+
+##attack = int(input("공격력은? "))
+##sheld = float(input("배율은? "))
+##
+##print("공격력과 배율을 곱한 값은", attack * sheld, "입니다!")
+
+
+##minute = int(input("분을 입력하세요 : "))
+##second = (minute * 60)
+##print(second, '초')
+
+problem1 = float(input("실수를 입력하세요 : "))
+print(f"{problem1 * 10:.3f}")
+
+
