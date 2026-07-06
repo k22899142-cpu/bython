3e663dc0e61b828a0f24f24edab0fbeaa1927d97
김범서 <k22899142@gmail.com>
2026-07-06 16:51:55 +0900

    Add files via upload

---
+++ 0706.py
@@ -0,0 +1,109 @@
+##passwd = input("비밀번호:  ")
+##print((input("id : "),"입력"))
+
+##passwd = input("비밀번호 : ")
+##print(type(passwd))
+
+
+##int(정수로 변환하고 싶은 데이터)
+
+##print(int("30000"),int(50.0),int(True))
+##      
+##
+##print(float("3.14"),float(5),float(False))
+##
+##
+##print(str(5),str(1.23),str(True))
+##
+
+
+##egg = input("계란은 몇 개?")
+##print(egg, type(egg))
+
+##egg = int(input("계란은 몇 개?"))
+##print(egg,type(egg))
+
+
+##egg = input("계란은 몇 개?")
+##egg = int(egg)
+##print(egg,type(egg))
+
+##egg = input("계란은 몇 개?")
+##egg = int(egg)
+##print(egg)
+
+##attack = input("공격력은?")
+##scale = input("배율은?")
+##
+##print("데미지는 ", attack*scale)
+
+##attack = float(input("공격력은?"))
+##scale = float(input("배율은?"))
+##
+##print("데미지는 ", attack*scale)
+
+##attack = int(input("공격력은?"))
+##scale = float(input("배율은?"))
+##
+##print("데미지는 ", attack*scale)
+
+##minute = int(input("분을 입력하세요 :"))
+##second = 60*minute
+##
+##text = str(minute)+"분="+ str(second)+"초"
+##print(text)
+
+
+
+###문제1#
+##word = input("닉네임을 입력하세요 : ")
+##word = str(word)+"님!"
+##print("환영합니다", word)
+##
+##
+##
+###문제2#
+##number = input("실수를 입력하세요 : ")
+##number = float(number)
+##print(f"{number * 10:.3f}")
+##
+##
+##
+###문제3#
+##number1 = int(input("첫번째 숫자를 입력하세요 : "))
+##number2 = int(input("두번째 숫자를 입력하세요 : "))
+##number3 = int(input("세번째 숫자를 입력하세료 : "))
+##
+##print(number1+number2+number3)
+##
+##
+##
+###문제4#
+##r = input("반지름을 입력하세요 : ")
+##r = int(r)
+##area = r * r * 3.14
+##length = f"{2 * r * 3.14:.1f}"
+##
+##print("원의 넓이는", area)
+##print("원의 둘레길이는",length)
+##
+##
+##
+###문제5#
+##u = int(input("윗변을 입력하세요 : "))
+##d = int(input("밑변을 입력하세요 : "))
+##h = int(input("높이를 입력하세요 : "))
+##
+##area = f"{( u + d ) * h / 2:.1f}"
+##  
+##print("사다리꼴의 넓이는", area )
+
+
+nut1 = '밤양갱'
+nut2 = '달디단 밤양갱,'
+nut3 = '달디달고 달디달고,'
+nut4 = '내가 먹고 싶었던 건'
+nut5 = '이야'
+print(nut3, nut2, nut1, '\n')
+print(nut4, nut2, nut1+nut5)
+
