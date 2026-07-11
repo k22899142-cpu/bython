837a54b5ff5e0b7ebac02c116a8f979eb8b5c603
김범서 <k22899142@gmail.com>
2026-07-11 23:41:15 +0900

    Add files via upload

---
+++ 0711 home.py
@@ -0,0 +1,84 @@
+##name1 = input("아이디: ")
+##PW1 = input("비밀번호: ")
+##
+##print('당신의 아이디는 '+'"'+name1+'"'+'이며, 비밀번호는 ' + '"'+PW1+
+##      '"'+'입니다.')
+
+##word = "문자열과 인덱스"
+##print(word[0])
+##print(word[3])
+##print(word[5])
+##print(word[-2])
+
+##snack = "떡볶이 순대 튀김"
+##setmenu = snack[0] + snack[4] + snack[7]
+##
+##print(setmenu)
+
+
+##word = "부분만 바꾸려고 하면 에러가 나요"
+##print(word)
+##
+##word[0] = "수"
+##word = "새로 만들어 덮어쓰기는 가능"
+##print(word)
+##
+
+
+##word = "슬라이싱으로 다양하게 문자를 잘라봅시다"
+##print(word[0:4])
+##print(word[7:9])
+##print(word[5::])
+##print(word[:12])
+##print(word[::3])
+##print(word[::-3])
+
+
+##song = "록도닳 고르마 이산두백 과물해동"
+##reverse = song[::-1]
+##
+##print(reverse)
+
+
+##song = "동해물과 백두산이 마르고 닳도록"
+##part_song = song[:4]
+##print(part_song)
+##part_song = song[5:13]
+##print(part_song)
+##part_song = song[14:]
+##print(part_song)
+
+
+##str1 = str(input("문자열을 입력하세요: "))
+##print(str1[1::2])
+
+
+##code1 = input("주민등록 번호를 입력하세요: ")
+##print(code1[2:6])
+
+
+##code2 = "타파에벅서이썬스나짱만스"
+##print(code2[11], end = "")
+##print(code2[0], end = "")
+##print(code2[3], end = "")
+##print(code2[7], end = "")
+##print(code2[2], end = "")
+##print(code2[4], end = " ")
+##print(code2[10], end = "")
+##print(code2[8])
+##
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
