d760bbdcd0e38fad781c7f845b792815384f7343
김범서 <k22899142@gmail.com>
2026-07-21 13:53:06 +0900

    Add files via upload

---
+++ 0721hhh.py
@@ -0,0 +1,109 @@
+# typing homework (134p ~ 149p)
+
+##string_a = "{1} {2} {0}".format(10, 20, 30)
+##print(string_a)
+
+##a = "{}만원".format(5000)
+##b = "파이썬 열공하여 {}만원 만들기 ".format(5000)
+##c = "{} {} {}".format(3000, 4000, 5000)
+##d = "{} {} {}".format(1, "문자열", True)
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+
+##a = "{} {}".format(10, 20)
+##b = "{} {} {}".format(20,30,50)
+##c = "{} {} {} {}".format(10, 20, 30, 40, 50)
+##
+##print(a)
+##print(b)
+##print(c)
+
+##a = "{:d}".format(52)
+##b = "{:5d}".format(52)
+##c = "{:10d}".format(52)
+##d = "{:05d}".format(52)
+##e = "{:05d}".format(-52)
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+##print(e)
+
+
+##a = "{:+d}".format(52)
+##b = "{:+d}".format(-52)
+##c = "{: d}".format(52)
+##d = "{: d}".format(-52)
+##
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+
+
+##a = "{:+5d}".format(52)
+##b = "{:+5d}".format(-52)
+##c = "{:=+5d}".format(52)
+##d = "{:=+5d}".format(-52)
+##e = "{:+05d}".format(52)
+##f = "{:+05d}".format(-52)
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+##print(e)
+##print(f)
+
+##a = "{:f}".format(52.273)
+##b = "{:15f}".format(52.273)
+##c = "{:+15f}".format(52.273)
+##d = "{:+015f}".format(52.273)
+##
+##
+##print(a)
+##print(b)
+##print(c)
+##print(d)
+##
+##a = "{:15.3f}".format(52.273)
+##b = "{:15.2f}".format(52.273)
+##c = "{:15.1f}".format(52.273)
+##
+##
+##print(a)
+##print(b)
+##print(c)
+
+##a = 52.0
+##b = "{:g}".format(a)
+##print(a)
+##print(b)
+
+
+##a = "hello python world!"
+##print(a.upper())
+##print(a.lower())
+
+##a = """    안녕하세요
+##파이썬을 배워보아용."""
+##
+##print(a)
+##print(a.strip())
+##
+##a = "안녕하세요"
+##print(a.find("안녕"))
+
+
+
+
+
+
+
+
+
