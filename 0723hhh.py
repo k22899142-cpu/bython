a4579a27e2a0589c94675b4c430ed968e4ad17db
김범서 <k22899142@gmail.com>
2026-07-24 02:19:34 +0900

    Add files via upload

---
+++ 0723hhh.py
@@ -0,0 +1,83 @@
+##string = "Hello"
+##
+##string.upper()
+##
+##print(string)
+##print(string.upper())
+
+##a = input("첫 번째 숫자: ")
+##b = input("두 번째 숫자: ")
+##
+##
+##print("{} + {} = {}".format(a, b, int(a)+int(b)))
+
+##r = int(input("구의 반지름을 입력해주세요: "))
+##v = 4/3 * 3.141592 * r**3
+##a = 4 * 3.141592 * r**2
+##
+##print(f"구의 부피는 {v} 입니다.")
+##print(f"구의 겉넓이는 {a} 입니다.")
+
+##b = float(input("밑변의 길이를 입력해주세요: "))
+##a = float(input("높이의 길이를 입력해주세요: "))
+##c = float((a ** 2) + (b ** 2)) ** 0.5
+##
+##print(f"빗변의 길이는 {c} 입니다.")
+
+##num1 = int(input("첫 번째 숫자 입력: "))
+##num2 = int(input("두 번째 숫자 입력: "))
+##num3 = int(input("세 번째 숫자 입력: "))
+##
+##print(f"10000 += num1 -> {10000 + num1}")
+##print(f"10000 -= num2 -> {10000 - num2}")
+##print(f"10000 *= num3 -> {10000 * num3}")
+
+
+##txt = input("문자열 입력: ")
+##num = input("숫자 입력: ")
+##
+##txt2 = txt
+##txt += num
+##txt2 *= int(num)
+##
+##print(txt)
+##print(txt2)
+
+#1)
+##num1 = input("숫자 3개를 입력해주세요 : ").split( )
+##
+##a = 100
+##a -= int(num1[0])
+##a -= int(num1[1])
+##a -= int(num1[2])
+##
+##print(a)
+ 
+
+#2)
+##num1 = input("숫자를 입력해주세요: ")
+##
+##txt = "삐약"
+##txt *= int(num1)
+##print(txt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
